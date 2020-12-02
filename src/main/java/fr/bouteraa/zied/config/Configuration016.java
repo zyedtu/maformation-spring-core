c352fb21f4ac97764919e4e3075f69f3d8e5b9a3
zyedtu <zyedtu@yahoo.fr>
2020-12-02 14:08:32 +0100

    modif package

--- src/main/java/fr/bouteraa/zied/config/Configuration016.java
+++ src/main/java/fr/bouteraa/zied/config/Configuration016.java
@@ -1,4 +1,4 @@
-package fr.zied.bouteraa.config;
+package fr.bouteraa.zied.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
--- src/main/java/fr/bouteraa/zied/maformation_spring_ioc/MainIoc_016.java
+++ src/main/java/fr/bouteraa/zied/maformation_spring_ioc/MainIoc_016.java
@@ -3,7 +3,7 @@ package fr.bouteraa.zied.maformation_spring_ioc;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import fr.zied.bouteraa.config.Configuration016;
+import fr.bouteraa.zied.config.Configuration016;
 
 public class MainIoc_016 {
 
